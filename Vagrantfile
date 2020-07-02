4e0596455cc1147a428d4a78a8bcb30ec7b63fdc
David Taylor <david.taylor@mnfgroup.limited>
2020-07-03 06:58:37 +1000

    Provisioning completes with no errors

--- Vagrantfile
+++ Vagrantfile
@@ -1,12 +1,13 @@
 Vagrant.configure("2") do |config|
   config.vm.define "djangoapp" do |djangoapp|
-    config.vm.box = "centos/8"
+    config.vm.box = "bento/centos-8.1"
 	config.vm.provider "virtualbox" do |vb|
 		vb.memory = 1024
 		vb.cpus = 1
 	end
 	djangoapp.vm.network "forwarded_port", guest: 80, host: 80
 	djangoapp.vm.network "forwarded_port", guest: 443, host: 443
+	djangoapp.vm.provision "shell", path: "./setup-centos.sh"
 	djangoapp.vm.provision "shell", path: "./provision-djangoapp.sh"
   end
 end
--- provision-djangoapp.sh
+++ provision-djangoapp.sh
@@ -1,40 +1,7 @@
 # provision-djangoapp.sh
-#!/usr/bin/env bash
 
-# Configure script
 set -e # Stop script execution on any error
-echo ""; echo "-----------------------------------------"
-
-# Configure variables
-MYHOST=djangoapp
-TESTPOINT=google.com
-echo "- Variables set -"
-
-# Test internet connectivity
-ping -q -c5 $TESTPOINT > /dev/null 2>&1
- 
-if [ $? -eq 0 ]
-then
-	echo "- Internet Ok -"	
-else
-	echo "- Internet failed -"
-fi
-
-# Set system name
-echo "- Set name to $MYHOST -"
-hostnamectl set-hostname $MYHOST
-cat >> /etc/hosts <<EOF
-10.0.2.15	$MYHOST $MYHOST.localdomain
-EOF
-
-# Sync clock
-echo "- Sync Clock -"
-ntpdate 0.au.pool.ntp.org
-
-# Base OS update
-echo "- Update OS -"
-rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-centosofficial
-dnf -yqe 3 update > /dev/null
+echo ""; echo "---- Provisioning Environment ----"
 
 # Install tools
 echo "- Installing Tools -"
@@ -42,14 +9,55 @@ dnf -yqe 3 install net-tools bind-utils python3 httpd
 
 # Configure firewall
 echo "- Update Firewall -"
-firewall-cmd --permanent --add-service=httpd
+systemctl enable --now firewalld.service
+firewall-cmd --permanent --add-service=http
 firewall-cmd --permanent --add-service=https
 firewall-cmd --reload
 
-# Create django folder
+# Set up Django
+echo "---- Setting up Django ----"
+useradd django
+# Create django folders
 mkdir /opt/django
+mkdir /opt/django/media
+mkdir /opt/django/static
 
 # setup project
 cd /opt/django
-pip install virtualenv
+pip3 -q install virtualenv
+python3 -m virtualenv venv
+source ./venv/bin/activate
+pip3 -q install django
+chown -R apache:apache /opt/django
 
+# Setup apache server
+echo "---- Setting up Apache Server ----"
+cat > /etc/httpd/conf.d/django.cont <<EOF
+<VirtualHost *:80>
+	WSGIScriptAlias / /opt/django/django/wsgi.py
+	WSGIPythonPath /opt/django/
+	Alias /robots.txt /opt/django/static/robots.txt
+	Alias /favicon.ico /opt/django/static/favicon.ico
+
+	Alias /media/ /opt/django/media/
+	Alias /static/ /opt/django/static/
+
+	<Directory /opt/django/>
+	<Files wsgi.py>
+		Order deny,allow
+		Allow from all
+	</Files>
+	</Directory>
+    ErrorLog /var/log/httpd/django-error.log
+    CustomLog /var/log/httpd/django-access.log combined
+</VirtualHost>
+
+EOF
+
+systemctl enable --now httpd.service
+systemctl reload httpd.service
+
+echo "---- Environment setup complete ----"; echo ""
+echo "------------------------------------------"
+echo " With great power, comes great opportunity"
+echo "------------------------------------------"
